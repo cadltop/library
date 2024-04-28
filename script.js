c8d19978f9b41c813ddc64d1075fef482e0dab05
cadltop <158861368+cadltop@users.noreply.github.com>
2024-04-27 22:51:20 -0500

    Include 'Add book'  button.

--- script.js
+++ script.js
@@ -11,32 +11,39 @@ function showBookData() {
     const tableContent = document.querySelector('tbody');
     const newRow = document.createElement('tr');
     
-    for (let propertie in myLibrary[0]) {
+    for (let propertie in myLibrary[myLibrary.length - 1]) {
         const newColumn = document.createElement('td');
-        newColumn.textContent = myLibrary[0][propertie];
+        newColumn.textContent = myLibrary[myLibrary.length - 1][propertie];
         newRow.appendChild(newColumn);
     }
     tableContent.appendChild(newRow);
 }
 
 function addBookToLibrary() {
-    const title = 'foo';
-    const author = 'foo';
-    const pages = 'foo';
-    const read = 'foo';
+    let title = document.querySelector('.title-container input').value;
+    let author = document.querySelector('.author-container input').value;
+    let pages = document.querySelector('.pages-container input').value;
+    let read = document.querySelector('.read-container input').value;
     myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary();
-showBookData();
-
 const newBookBtn = document.querySelector('.new-book');
 const dialog = document.querySelector('dialog');
-const closeBtn = document.querySelector('.close')
+const closeBtn = document.querySelector('.close');
+const addBtn = document.querySelector('.add');
+const inputs = document.querySelectorAll('div input');
 
 newBookBtn.addEventListener('click', () => {
     dialog.showModal();
 });
 closeBtn.addEventListener('click', () => {
     dialog.close();
+});
+addBtn.addEventListener('click', () => {
+    addBookToLibrary();
+    dialog.close();
+    showBookData();
+    for (let i = 0; i < inputs.length; i++){
+        inputs[i].value = '';
+    }
 });
